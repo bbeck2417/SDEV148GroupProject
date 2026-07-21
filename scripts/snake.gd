00ea3174de05baeeff390430fad0af3a1f219973
bbeck2417 <billys.beck@gmail.com>
2026-07-20 20:33:53 -0400

    add snake direction and movement

--- scripts/snake.gd
+++ scripts/snake.gd
@@ -22,4 +22,30 @@ func reset():
 	
 func _ready():
 	reset()
-	print(body.size())
+
+func request_direction(new_direction):
+	if not turn_ready:
+		return
+		
+	if new_direction == -current_direction:
+		return
+		
+	if new_direction == current_direction:
+		return
+		
+	next_direction = new_direction
+	turn_ready = false
+	
+func get_next_head_position():
+	return body[0] + next_direction
+	
+func advance(growing):
+	var new_head = get_next_head_position()
+	body.push_front(new_head)
+	
+	if not growing:
+		body.pop_back()
+	
+	current_direction = next_direction
+	turn_ready = true
+	queue_redraw()
